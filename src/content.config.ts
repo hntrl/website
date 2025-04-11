2da6c04721e3c2a630c60f8690aa3108ea8f907d
hntrl <hunter@hntrl.io>
2025-04-11 14:31:27 -0600

    chore: fix blog post page

--- src/content.config.ts
+++ src/content.config.ts
@@ -1,9 +1,7 @@
-import { glob } from "astro/loaders";
 import { defineCollection, z } from "astro:content";
 
 const blog = defineCollection({
-  // Load Markdown and MDX files in the `src/content/blog/` directory.
-  loader: glob({ base: "./src/content/blog", pattern: "**/*.{md,mdx}" }),
+  type: "content",
   // Type-check frontmatter using a schema
   schema: z.object({
     title: z.string(),
