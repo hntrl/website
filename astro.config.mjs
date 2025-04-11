e1c1ac6292aead7d868453a2d72176cf5cd1e1cb
hntrl <hunter@hntrl.io>
2025-04-10 20:57:14 -0600

    chore: cleanup config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,9 @@
-import { defineConfig, passthroughImageService } from "astro/config";
+import { defineConfig } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
+
 import mdx from "@astrojs/mdx";
-
 import icon from "astro-icon";
-
 import react from "@astrojs/react";
-
 import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
